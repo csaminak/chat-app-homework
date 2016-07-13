a44683e21784b59f4d1fdf5304320d41c27c190e
Samina Chhatwal <csaminak@gmail.com>
2016-07-13 14:27:03 -0400

    set up initial load message

--- client/js/chat-app.js
+++ client/js/chat-app.js
@@ -2,6 +2,16 @@
     'use strict';
     window.chat = ns = (ns || {});
 
-    
+    var $user;
+    var $msg;
+    var messageSection = $('.messages');
+
+    ns.listenForMessages(function messageHandler(data) {
+        $user = data.username;
+        $msg = data.message;
+        messageSection.append('<p>' + $user + ' ' + $msg + '</p>');
+    });
+
+
 
 })(window.chat);
